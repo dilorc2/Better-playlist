564e21e9b796a13356d8de886fbe1e2597736761
Carlo DiLorenzo <dilorc2@gmail.com>
2019-07-19 11:50:13 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Aggregate() {
+    return (
+        <div style={{width: "40%", display: "inline-block"}}
+             className="aggregate"
+        >
+            <h2>Number Text</h2>
+        </div>
+    );
+}
+
+function Filter() {
+    return (
+        <div>
+            <input type="text"/>
+            Filter
+        </div>
+    );
+}
+
+function Playlist() {
+    return (
+        <div style={{width:"25%", display:"inline-block"}}>
+            <h3>Playlist Name</h3>
+            <ul>
+                <li>Song 1</li>
+                <li>Song 2</li>
+                <li>Song 3</li>
+            </ul>
+        </div>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
     </div>
   );
 }
