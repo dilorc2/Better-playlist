8fcd877227f2d68bb6358e3a34223e7f669905e5
Carlo DiLorenzo <dilorc2@gmail.com>
2019-07-21 17:50:09 -0700

    updated filter

--- src/App.js
+++ src/App.js
@@ -1,51 +1,136 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function Aggregate() {
-    return (
-        <div style={{width: "40%", display: "inline-block"}}
-             className="aggregate"
-        >
-            <h2>Number Text</h2>
-        </div>
-    );
+let defaultStyle = {
+    color: 'black'
+};
+let fakeServerData = {
+    user: {
+        name: 'Carlo',
+        playlists: [
+            {
+                name: 'My favorites',
+                songs: [
+                    {name: 'Beat It', duration: 1345},
+                    {name: 'Cannelloni Makaroni', duration: 1236},
+                    {name: 'Rosa helikopter', duration: 70000}
+                ]
+            },
+            {
+                name: 'Discover Weekly',
+                songs: [
+                    {name: 'Beat It', duration: 1345},
+                    {name: 'Cannelloni Makaroni', duration: 1236},
+                    {name: 'Rosa helikopter', duration: 70000}
+                ]
+            },
+            {
+                name: 'Another playlist - the best!',
+                songs: [
+                    {name: 'Beat It', duration: 1345},
+                    {name: 'Cannelloni Makaroni', duration: 1236},
+                    {name: 'Rosa helikopter', duration: 70000}
+                ]
+            },
+            {
+                name: 'Playlist - yeah!',
+                songs: [
+                    {name: 'Beat It', duration: 1345},
+                    {name: 'Cannelloni Makaroni', duration: 1236},
+                    {name: 'Rosa helikopter', duration: 70000}
+                ]
+            }
+        ]
+    }
+};
+
+class PlaylistCounter extends Component {
+    render() {
+        return (
+            <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+                <h2>{this.props.playlists.length} playlists</h2>
+            </div>
+        );
+    }
 }
 
-function Filter() {
-    return (
-        <div>
-            <input type="text"/>
-            Filter
-        </div>
-    );
+class HoursCounter extends Component {
+    render() {
+        let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+            return songs.concat(eachPlaylist.songs)
+        }, [])
+        let totalDuration = allSongs.reduce((sum, eachSong) => {
+            return sum + eachSong.duration
+        }, 0)
+        return (
+            <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+                <h2>{Math.round(totalDuration/60)} hours</h2>
+            </div>
+        );
+    }
 }
 
-function Playlist() {
-    return (
-        <div style={{width:"25%", display:"inline-block"}}>
-            <h3>Playlist Name</h3>
-            <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
-                <li>Song 3</li>
-            </ul>
-        </div>
-    );
+class Filter extends Component {
+    render() {
+        return (
+            <div style={defaultStyle}>
+                <input type="text" onKeyUp={event =>
+                    this.props.onTextChange(event.target.value)}/>
+            </div>
+        );
+    }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Title</h1>
-      <Aggregate/>
-      <Aggregate/>
-      <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-    </div>
-  );
+class Playlist extends Component {
+    render() {
+        let playlist = this.props.playlist
+        return (
+            <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
+                <h3>{playlist.name}</h3>
+                <ul>
+                    {playlist.songs.map(song =>
+                        <li>{song.name}</li>
+                    )}
+                    </ul>
+            </div>
+        );
+    }
 }
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            serverData: {},
+            filterString:''
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({serverData: fakeServerData});
+        }, 1000);
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.state.serverData.user ?
+                    <div>
+                        <h1 style={{...defaultStyle, 'font-size': '54px'}}>
+                            {this.state.serverData.user.name}'s Playlists
+                        </h1>
+                        <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+                        <HoursCounter playlists={this.state.serverData.user.playlists}/>
+                        <Filter onTextChange={text => this.setState({filterString: text})}/>
+                        {this.state.serverData.user.playlists.filter(playlist =>
+                            playlist.name.toLowerCase().includes(
+                                this.state.filterString.toLowerCase())
+                        ).map(playlist =>
+                            <Playlist playlist={playlist}/>
+                        )}
+                    </div> : <h1 style={defaultStyle}>Loading...</h1>
+                }
+            </div>
+        );
+    }
+}
 export default App;
